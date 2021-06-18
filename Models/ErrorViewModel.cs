493147fc8612803ba5dd422b08b938bce88736cc
Artem Konkin <temakonkin@gmail.com>
2021-06-18 12:38:05 +0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DIWOS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
